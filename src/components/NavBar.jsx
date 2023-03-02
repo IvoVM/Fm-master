549124777f9ee7c06e7050ecb0a502ea133a6c4f
IvoVM <ivomastrangelo12@gmail.com>
2023-03-02 18:51:23 -0300

    Modified Navbar to use SmoothScroll

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,74 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import redIcon from "../assets/images/red-circle.png";
+import {
+  Link,
+  animateScroll as scroll,
+} from "react-scroll";
+
 function NavBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="p-3">
-      <Navbar.Brand href="#home">
-        <span className="master">Master</span>105.3{" "}
-        <span className="fm">FM</span>
+      <Navbar.Brand>
+        <Link  className="nav-link" to="home" spy={true} smooth={false} offset={-100} duration={500}>
+          <span className="master">Master</span>105.3{" "}
+          <span className="fm">FM</span>
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav >
-          <Nav.Link href="#0" className="live d-flex align-items-center">
-            <img src={redIcon} /> <p>Escuchar en vivo</p>
-          </Nav.Link>
-          <Nav.Link href="#1">Nuestra Historia</Nav.Link>
-          <Nav.Link href="#2">Familia Master</Nav.Link>
-          <Nav.Link href="#3">Calendario</Nav.Link>
-          <Nav.Link href="#4">Formar Parte</Nav.Link>
+        <Nav className="text-white">
+          <Link
+            to="players"
+            spy={true}
+            smooth={false}
+            offset={-100}
+            duration={500}
+            className="nav-link d-flex live"
+          >
+            <img className="mt-2" src={redIcon} />
+            <p className="m-0">Escuchar en vivo</p>
+          </Link>
+          <Link
+            to="about"
+            spy={true}
+            smooth={false}
+            offset={-150}
+            duration={200}
+            className="nav-link d-flex align-items-center"
+          >
+            Nuestra Historia
+          </Link>
+          <Link
+            to="teamcard"
+            spy={true}
+            smooth={false}
+            offset={-100}
+            duration={500}
+            className="nav-link d-flex align-items-center"
+          >
+            Familia Master
+          </Link>
+          <Link
+            to="schedule"
+            spy={true}
+            smooth={false}
+            offset={-100}
+            duration={500}
+            className="nav-link d-flex align-items-center"
+          >
+            Programación
+          </Link>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={false}
+            offset={-100}
+            duration={500}
+            className="nav-link d-flex align-items-center"
+          >
+            Contactarse
+          </Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
