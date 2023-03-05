58e60471481722f63298f845385c6114b0916ba9
IvoVM <ivomastrangelo12@gmail.com>
2023-03-04 22:29:24 -0300

    Created ContactMe component

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ export {default as Carrousel} from './Carrousel';
 export {default as Players} from './Players';
 export {default as AboutUs} from './AboutUs';
 export {default as TeamCards} from './TeamCards';
+export {default as ContactMe} from './ContactMe';
+
 
 
 
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,5 +1,5 @@
 import Container from 'react-bootstrap/Container';
-import { NavBar,Carrousel,Players,AboutUs,TeamCards } from "../components";
+import { NavBar,Carrousel,Players,AboutUs,TeamCards,ContactMe } from "../components";
 const Main = () => {
   return (
     <Container fluid className='p-0'>
@@ -8,6 +8,7 @@ const Main = () => {
       <Players />
       <AboutUs />
       <TeamCards />
+      <ContactMe />
     </Container>
       
   );
