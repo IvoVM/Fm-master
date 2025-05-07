46b3fd6fe0e8a94bc2e7103307893f1dd46347f4
Ivo Mastrangelo <ivomastrangelo12@gmail.com>
2025-05-07 09:09:55 -0300

    Reverted changes to semanario section

--- src/components/Semanario.jsx
+++ src/components/Semanario.jsx
@@ -1,26 +1,12 @@
-import img from "../assets/images/semanario-logo.png";
-import fiestaLogo from "../assets/images/fiesta_25_años.jpg";
-
+import img from '../assets/images/semanario-logo.png';
 const Semanario = () => {
-  return (
-    <section className="row px-1  px-xxl-5  mt-5" id="semanario">
-      <div className="col-12 col-xxl-4  pt-5 px-1  px-xxl-5 d-flex justify-content-center">
-        <img src={fiestaLogo} height="100%" width="100%" className="h-100-responsive" />
-      </div>
-      <div className="col-12 col-xxl-8 d-flex align-items-center flex-column  mt-5 mt-xxl-0">
-        <img src={img} />
-        <h4 className="mb-5 text-center ">
-          Enterate de las últimas noticias de{" "}
-          <span className="rose">Miramar.</span>
-        </h4>
-        <iframe
-          className="semanario "
-          width="100%"
-          height="100%"
-          src="https://elsemanariomiramar.com.ar/"
-        ></iframe>
-      </div>
-    </section>
-  );
-};
+    return (
+        <section id="semanario"  className="d-flex justify-content-center align-items-center flex-column mt-5">
+            <img src={img}/>
+            <h4 className='mb-5 text-center mt-4'>Enterate de las últimas noticias de <span className="rose">Miramar.</span></h4>
+            <iframe  className="semanario" width="1100px" height="450px" src="https://elsemanariomiramar.com.ar/"></iframe>
+        </section>
+    )
+
+}
 export default Semanario;
