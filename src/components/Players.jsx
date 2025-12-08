bf6fd7a7eb0fa69d1015269f0943edd86ba5cfc1
Ivo <ivo@etnaeducacion.com.ar>
2025-12-07 21:40:57 -0300

    added new circus images

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,18 +1,49 @@
 import Player from "./Player";
 import { players } from "../utils/utils";
 import logo from "../assets/images/grupo-master-logo.jpeg";
-
+import publiCirco from "../assets/images/publi_circo.jpeg";
+import publiCirco2 from "../assets/images/publi_circo_2.jpeg";
 const Players = () => {
   return (
-    <section id="players" className="row mt-4 px-3" >
-      <img src={logo}  height="300px" className="rounded-5 w-50 col d-none d-md-block" />
-
-      <div className="d-flex justify-content-center align-items-center col" >
+    <section id="players" className="row mt-4 px-3 g-3 align-items-center">
+      {" "}
+      {/* Publicidad circo izquierda */}{" "}
+      <div className="col-12 col-md-2 d-flex justify-content-center">
+        {" "}
+        <img
+          src={publiCirco}
+          alt="Publicidad del circo"
+          className="img-fluid"
+          style={{ maxHeight: "300px", objectFit: "contain" }}
+        />{" "}
+      </div>{" "}
+      {/* Logo */}{" "}
+      <div className="col-12 col-md-4 d-flex justify-content-center">
+        {" "}
+        <img
+          src={logo}
+          height="300px"
+          className="rounded-5 img-fluid d-none d-md-block"
+          alt="Logo"
+        />{" "}
+      </div>{" "}
+      {/* Players */}{" "}
+      <div className="col-12 col-md-4 d-flex justify-content-center align-items-center">
+        {" "}
         {players.map((item) => (
           <Player key={item.id} info={item} />
-        ))}
-      </div>
-
+        ))}{" "}
+      </div>{" "}
+      {/* Publicidad circo derecha */}{" "}
+      <div className="col-12 col-md-2 d-flex justify-content-center">
+        {" "}
+        <img
+          src={publiCirco2}
+          alt="Publicidad del circo 2"
+          className="img-fluid"
+          style={{ maxHeight: "300px", objectFit: "contain" }}
+        />{" "}
+      </div>{" "}
     </section>
   );
 };
